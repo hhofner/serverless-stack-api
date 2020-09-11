67abe95ddd738476a33cc04ed07b43f595bc5d1e
Hans Hofner <hhofner@gmail.com>
2020-09-11 10:08:58 +0900

    refactor create script

--- create.js
+++ create.js
@@ -4,24 +4,11 @@ import dynamoDb from "./libs/dynamodb-lib";
 import AWS from "aws-sdk";
 
 AWS.config.update({ region: "us-east-2" });
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export function main(event, context, callback) {
-  // Request body is passed in as a JSON encoded string
-  // in 'event.body
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
-    TableName: process.env.tableName,
-    // 'Item' contains the attributes of the item to be
-    // created
-    // - 'userId': user identifies are federated through
-    // the Cognito Identity Pool, we will use the identity id as the user id of the authenticated user
-    // - 'noteId': a unique uuid
-    // - 'content': parsed from request body
-    // - 'attachment': parsed from request body
-    // - 'createdAt': current Unix timestamp
-    //
+    TableName: process.env.tablename,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
@@ -31,30 +18,8 @@ export function main(event, context, callback) {
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
+  await dynamoDb.put(params);
 
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
+  return params.Item;
 
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
-}
+});

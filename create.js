2d33771730dea7e52f329c8594ae99f211309378
Hans Hofner <hhofner@gmail.com>
2020-09-13 16:26:18 +0900

    fix bug

--- create.js
+++ create.js
@@ -1,14 +1,11 @@
 import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
-import AWS from "aws-sdk";
-
-AWS.config.update({ region: "us-east-2" });
 
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: process.env.tablename,
+    TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
